330a0971b2aa2b088ba461ffa68069604a583884
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-02-10 17:39:10 +0100

    Add balance checks

--- src/app/api/multisend/route.ts
+++ src/app/api/multisend/route.ts
@@ -1,36 +1,68 @@
-import { defaultParser, buildMetaTransaction } from "multi-asset-transfer";
-import { signRequestFor } from "@bitte-ai/agent-sdk";
+import {
+  defaultParser,
+  buildMetaTransactions,
+  checkAllBalances,
+  getFungibleBalance,
+  getCollectibleBalance,
+} from "multi-asset-transfer";
+import {
+  FieldParser,
+  signRequestFor,
+  numberField,
+  addressField,
+  validateInput,
+} from "@bitte-ai/agent-sdk";
 import { NextRequest, NextResponse } from "next/server";
+import { Address } from "viem";
+
+interface Input {
+  chainId: number;
+  safeAddress: Address;
+}
+
+const parsers: FieldParser<Input> = {
+  chainId: numberField,
+  safeAddress: addressField,
+};
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const { searchParams } = request.nextUrl;
   console.log("MultiSend Request", searchParams);
+  const { chainId, safeAddress } = validateInput<Input>(searchParams, parsers);
   const csv = searchParams.get("csv");
   // TODO: Validate CSV and allow fetch from URL.
   if (!csv) {
     return NextResponse.json({ error: "CSV is required" }, { status: 400 });
   }
-  const chainId = searchParams.get("chainId");
-  if (!chainId) {
-    return NextResponse.json(
-      { error: "Chain ID is required" },
-      { status: 400 },
-    );
-  }
-  const parser = defaultParser(Number(chainId));
+
+  const parser = defaultParser(chainId);
   const [transfers, warnings] = await parser(csv);
   if (warnings.length > 0) {
     console.warn("Parser Warnings", warnings);
   }
-  const txs = buildMetaTransaction(transfers);
+  const [fungibleBalances, nftBalances] = await Promise.all([
+    getFungibleBalance(chainId, safeAddress),
+    getCollectibleBalance(chainId, safeAddress),
+  ]);
+  const insufficientBalances = checkAllBalances(
+    fungibleBalances,
+    nftBalances,
+    transfers,
+  );
+  if (insufficientBalances.length > 0) {
+    console.warn("Insufficient balance warning", insufficientBalances);
+  }
 
   const signRequest = signRequestFor({
-    chainId: Number(chainId),
-    metaTransactions: txs,
+    chainId,
+    metaTransactions: buildMetaTransactions(transfers),
   });
   console.log("Sign Request", signRequest);
   return NextResponse.json({
     transaction: signRequest,
-    meta: { transfers, warnings },
+    meta: {
+      transfers,
+      warnings: { parse: warnings, balance: insufficientBalances },
+    },
   });
 }
