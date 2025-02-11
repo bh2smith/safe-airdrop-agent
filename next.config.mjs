047afd0e1ce4857b5c4d3b87fb237bd9f26ba6db
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-02-11 16:17:50 +0100

    Fix CORS for Local Dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
