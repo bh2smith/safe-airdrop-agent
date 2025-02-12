0f293af66bf63e9d42ddd3627b40d46a70805ee9
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-02-12 21:00:52 +0100

    Add Drain Safe Route (#7)

--- src/app/api/drain-safe/route.ts
+++ src/app/api/drain-safe/route.ts
@@ -6,15 +6,22 @@ import {
 } from "@bitte-ai/agent-sdk";
 import { NextRequest, NextResponse } from "next/server";
 import { Address } from "viem";
-import { buildResponse, csvAirdrop } from "./flow";
+import {
+  drainSafe,
+  getCollectibleBalance,
+  getFungibleBalance,
+} from "multi-asset-transfer";
+import { buildResponse, csvAirdrop } from "../multisend/flow";
 
 interface Input {
   chainId: number;
+  recipient: Address;
   safeAddress: Address;
 }
 
 const parsers: FieldParser<Input> = {
   chainId: numberField,
+  recipient: addressField,
   safeAddress: addressField,
 };
 
@@ -22,12 +29,21 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   console.log("Request Headers", request.headers);
   const { searchParams } = request.nextUrl;
   console.log("MultiSend Request", searchParams);
-  const { chainId, safeAddress } = validateInput<Input>(searchParams, parsers);
+  const { chainId, recipient, safeAddress } = validateInput<Input>(
+    searchParams,
+    parsers,
+  );
+  const [ft, nft] = await Promise.all([
+    getFungibleBalance(chainId, safeAddress, true, true),
+    getCollectibleBalance(chainId, safeAddress, true, true),
+  ]);
+
+  const csv = drainSafe(recipient, { ft, nft });
   const response = await csvAirdrop(
     chainId,
     safeAddress,
-    searchParams.get("csv"),
+    csv,
+    true, // Skip Balance Check.
   );
-
   return buildResponse(chainId, response);
 }
